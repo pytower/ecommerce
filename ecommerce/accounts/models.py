eeb64ef88985f34e80366e56b063faba71335fbf
Amir Pashayi <amirpashayi.dev@gmail.com>
2025-04-13 14:57:16 +0330

    Organize imports using isort

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -1,6 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser, PermissionsMixin
-from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
+from django.db import models
+
 from .managers import UserManager
 
 
